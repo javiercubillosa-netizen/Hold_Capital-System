bc4282158e518160d6733adf189622c8eb62e629
javiercubillosa-netizen <javier.cubillosa@gmail.com>
2026-03-24 09:02:17 -0500

    Fix merge conflict in app.py
    Removed conflicting code related to port assignment.

--- holdcapital.io/app.py
+++ holdcapital.io/app.py
@@ -11,7 +11,6 @@ def home():
     return render_template('index.html')
 
 if __name__ == "__main__":
-<<<<<<< HEAD
     port = int(os.environ.get("PORT", 8080))
     app.run(host='0.0.0.0', port=port)
 =======
